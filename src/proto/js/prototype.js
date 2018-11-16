597f6d102900025da54ab709d14d1b61564769ea
ernscht <ernscht@gmail.com>
2018-11-16 13:58:04 +0100

    update 4.1.6

--- src/proto/js/prototype.js
+++ src/proto/js/prototype.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
-console.log('You are using nitro version 4.1.5');
+console.log('You are using nitro version 4.1.6');
 console.log('Look for updates on https://npmjs.com/package/generator-nitro');
 /* eslint-enable no-console */
