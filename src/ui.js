a627ceb0f4ba07056a39a68e48c639a189ea541f
ernscht <ernscht@gmail.com>
2018-12-05 11:20:59 +0100

    update 4.1.9

--- src/ui.js
+++ src/ui.js
@@ -16,12 +16,14 @@ import './patterns/atoms/box';
 import './patterns/atoms/button';
 import './patterns/atoms/checkbox';
 import './patterns/atoms/cta';
+import './patterns/atoms/datepicker';
 import './patterns/atoms/gondel';
 import './patterns/atoms/heading';
 import './patterns/atoms/icon';
 import './patterns/atoms/image';
 import './patterns/atoms/list';
 import './patterns/atoms/loader';
+import './patterns/atoms/lottie';
 import './patterns/atoms/stage';
 import './patterns/molecules/example';
 
