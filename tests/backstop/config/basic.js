df8f5df4748a54507b27549eb4c64b1a8f4eeca1
ernscht <ernscht@gmail.com>
2019-09-08 20:14:20 +0200

    ci: detect ci

--- tests/backstop/config/basic.js
+++ tests/backstop/config/basic.js
@@ -3,6 +3,8 @@
 const port = process.env.PORT || '8889';
 const hostName = process.env.CI ? 'http://localhost' : 'http://host.docker.internal';
 const host = `${hostName}:${port}`;
+console.log(`backstop test host: ${host}`);
+console.log(process.env);
 const viewports = [
 	{
 		label: 'phone',
