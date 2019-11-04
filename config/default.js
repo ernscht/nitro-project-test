39ad66c256e86fb05e9132a62c3e4d3ea1e192b0
ernscht <ernscht@gmail.com>
2019-11-04 21:38:29 +0100

    release 4.14.2

--- config/default.js
+++ config/default.js
@@ -29,7 +29,6 @@ const defaultConfig = {
 	nitro: {
 		viewFileExtension: 'hbs',
 		templateEngine: 'hbs',
-		jsCompiler: 'js',
 		patterns: require('./default/patterns'),
 		mode: {
 			livereload: true,
