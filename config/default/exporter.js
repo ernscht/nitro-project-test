8acead20bda9edeadf8b77949fda321bb5608e64
ernscht <ernscht@gmail.com>
2021-01-25 20:44:08 +0100

    release 5.8.4

--- config/default/exporter.js
+++ config/default/exporter.js
@@ -39,10 +39,6 @@ const config = {
 					glob: ['export/*/css/*.css'],
 					replace: [
 						{
-							from: '/assets/(light|dark)/',
-							to: '../',
-						},
-						{
 							from: '/content/',
 							to: '../content/',
 						},
