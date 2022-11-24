12548beec2845080af29098f6fe46b73ce8c2b60
ernscht <ernscht@gmail.com>
2022-11-24 12:35:37 +0100

    release 7.2.0

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,11 @@
-const lintConfig = require('@namics/stylelint-config');
+const lintConfig = require('@merkle-open/stylelint-config');
 
 lintConfig.rules['plugin/stylelint-bem-namics'] = {
 	patternPrefixes: ['a', 'm', 'o', 'h'],
 	helperPrefixes: ['state'],
 };
+
+// node-sass can't handle modern color function notation
+lintConfig.rules['color-function-notation'] = null;
 
 module.exports = lintConfig;
